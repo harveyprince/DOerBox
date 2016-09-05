95af5951d031c8757d403e1a8ea7a55f730bb891
harveyprince <948841233@qq.com>
2016-09-05 14:18:24 +0800

    update

--- js/component/searchbar.js
+++ js/component/searchbar.js
@@ -3,13 +3,17 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  View
+  View,
+  Image
 } from 'react-native';
 
 export class SearchBar extends Component {
     render() {
       return (
         <View style={styles.container}>
+          <Image style={styles.icon}
+            source={require('../image/search_black.png')}
+          />
           <TextInput style={styles.searchbox} />
         </View>
       )
@@ -18,12 +22,19 @@ export class SearchBar extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     alignItems: 'center',
     alignSelf: 'stretch',
     backgroundColor: '#F5FCFF',
   },
+  icon: {
+    
+    width: 30
+  },
   searchbox: {
+    flex: 1,
+    alignSelf: 'stretch',
     height: 50,
-    alignSelf: 'stretch'
+    
   }
 });
