c6b087fdaa0aa896c29db906b55fdcb6873c6a72
harveyprince <948841233@qq.com>
2016-09-06 13:38:04 +0800

    send icon click

--- js/component/searchbar.js
+++ js/component/searchbar.js
@@ -4,7 +4,8 @@ import {
     Text,
     TextInput,
     View,
-    Image
+    Image,
+    TouchableOpacity
 } from 'react-native';
 import {connect} from 'react-redux';
 import {addTodo} from '../action/todo';
@@ -12,22 +13,40 @@ import {addTodo} from '../action/todo';
 let idx = 0;
 
 class SearchBar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text: ''
+        };
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Image style={styles.icon}
-                       source={require('../image/search_black.png')}
-                />
+
                 <TextInput style={styles.searchbox}
-                           onChangeText={(text) => {
-                               console.log(text);
-                               this.props.dispatch(addTodo({
-                                   id: idx ++,
-                                   content: text,
-                                   tag_finish: false
-                               }))
-                           }}
+                           ref='todo_input'
+                           onChangeText={(text) => this.setState({text})}
+                           value={this.state.text}
                 />
+                <TouchableOpacity
+                    onPress={()=>{
+                        let text = this.state.text;
+                        if (text) {
+                            this.props.dispatch(addTodo({
+                                id: idx ++,
+                                content: text,
+                                tag_finish: false
+                            }));
+                        }
+                        this.setState({text:''});
+
+                    }}
+                >
+                    <Image style={styles.icon}
+                           source={require('../image/send_black.png')}
+                    />
+                </TouchableOpacity>
+
             </View>
         )
     }
@@ -43,7 +62,10 @@ const styles = StyleSheet.create({
     },
     icon: {
 
-        width: 30
+        width: 30,
+        height: 30,
+        marginLeft: 10,
+        marginRight: 10
     },
     searchbox: {
         flex: 1,
